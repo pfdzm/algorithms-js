7098d58801b85bbc6129855df4e7450487b15bb4
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-12-22 12:05:25 +0100

    use map to solve fizzbuzz problem

---
+++ fizz-buzz/fizz-buzz.js
@@ -0,0 +1,25 @@
+// Write code to loop through the array of numbers
+// At each iteration, if a number is evenly divisible by 3 print "Fizz"
+// If a number is evenly divisible by 5 print "Buzz"
+// If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
+// If a number is not divisible by 3 or 5, print the number
+
+var fizzBuzz = function(arr) {
+  return arr.map(fizzer);
+};
+
+let fizzer = function(num) {
+  if (num % 3 === 0) {
+    if (num % 5 === 0) {
+      console.log("Fizz Buzz");
+    } else {
+      console.log("Fizz");
+    }
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  }
+};
+
+fizzBuzz([13, 14, 15, 16, 17, 18, 19, 20]);
