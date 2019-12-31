97a60224a3bfa065225f1178ccc21b5728dbe466
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-12-31 11:02:35 +0100

    change regex to make the first letter lowercase

--- camel-case/camel-case.js
+++ camel-case/camel-case.js
@@ -1,10 +1,10 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
 var camelCase = function(str) {
-  const regex = /(^|\s)([a-z])/gi;
+  const regex = /(^[a-z])(|\s[a-z])/gi;
   return str.replace(regex, camelCasify);
 };
 
-const camelCasify = (match, $1, $2) => $2.toUpperCase();
+const camelCasify = (match, $1, $2) => $1.toLowerCase() || $2.toUpperCase();
 
 console.log(camelCase("A Lannister always pays his debts"));
